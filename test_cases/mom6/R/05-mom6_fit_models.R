33a3f3df9cbecbc6bc875ccedee0b67fafcad16e
LivGemmell <oliviagemmell@shaw.ca>
2025-09-11 15:16:05 -0400

    Switched to scaled and untransformed version

--- test_cases/mom6/R/05-mom6_fit_models.R
+++ test_cases/mom6/R/05-mom6_fit_models.R
@@ -83,19 +83,22 @@ plotmodel = T
 
 
 #Scale?
-scale <- F
+scale <- TRUE
 if(scale==T){
   dat$o2 <- dat$o2/100
 }
+
+# transform response?
+transform <- "none"
 
 
 #Run model fit
 rmse_bc_cc <- mom6_fit(dat = dat, 
                        test_region = "bc_cc", 
                        root_dir = root_dir,
-                       scale = FALSE,
-                       transform = "quantile",
-                       barrier_mesh = TRUE)
+                       scale = TRUE,
+                       transform = "none",
+                       barrier_mesh = FALSE)
 
 # ------------------------------------------------------------------------------
 # look at results
@@ -105,8 +108,12 @@ preds_all <- read_rds(file = paste0(root_dir, "data/mom6/mom6_bc_cc.rds"))
 preds_all <- preds_all[preds_all$depth <= 500, ]
 preds_all <- preds_all[preds_all$o2 >= 0, ]
 preds_all$depth_ln <- log(preds_all$depth)
-# Apply ordered quantile normalization to o2 column
-bn <- orderNorm(preds_all$o2)
+
+if(transform == "quantile"){
+  # Apply ordered quantile normalization to o2 column
+  bn <- orderNorm(preds_all$o2)
+}
+
 
 rmse_bc_cc$rmse_summary
 
@@ -122,7 +129,8 @@ rmse_bc_cc$figures$fig_2011
 # and random fields are turned off in the predict function to isolate the depth
 # effect 
 plot_depth_response(mod = mod,
-                    bn = bn,
+                    transform = "none",
+                    scale = TRUE,
                     preds_all = preds_all,
                     year = 2011,
                     month_fixed = 2,
@@ -143,7 +151,8 @@ plot(sim_resids)
 rmse_bc_cc$figures$fig_2012
 # Plot response curve
 plot_depth_response(mod = mod,
-                    bn = bn,
+                    transform = "none",
+                    scale = TRUE,
                     preds_all = preds_all,
                     year = 2012,
                     month_fixed = 2,
